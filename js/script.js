1dd42bf5257bf6e6cffee3b2dc43c2c2bf684e4c
Manoel Gildarc <Manoelgildarc@hotmail.com>
2023-07-04 21:46:11 -0300

    Adicionado a variável pontos.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 function jokenpo(jogador1){
     let cpu = Math.floor(Math.random() * (3 - 1 + 1)) + 1
+    let pontos = parseFloat(document.getElementById('ponto'))
     if(jogador1 == cpu){
         return 'Draw!'
     } else if (jogador1 == 1 && cpu == 2){
+        pontos--
         return 'You Lose!'
     }else if (jogador1 == 1 && cpu == 3){
+        pontos++
         return 'You Win!'
     }else if (jogador1 == 2 && cpu == 1){
+        pontos++
         return 'You Win!'
     }else if (jogador1 == 2 && cpu == 3){
+        pontos--
         return 'You Lose!'
     }else if (jogador1 == 3 && cpu == 1){
+        pontos--
         return 'You Lose!'
     }else if (jogador1 == 3 && cpu == 2){
+        pontos++
         return 'You Win!'
     }
 }
