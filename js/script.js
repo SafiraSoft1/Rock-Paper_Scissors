83173c3612104ce16993e447a7a1a9d0a9ba33b2
Manoel Gildarc <Manoelgildarc@hotmail.com>
2023-07-05 20:36:28 -0300

    Corrigido variavel pontos.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
+let pontos = 0 /* parseFloat(document.getElementById('ponto')) */
+
 function jokenpo(jogador1){
     let cpu = Math.floor(Math.random() * (3 - 1 + 1)) + 1
-    let pontos = parseFloat(document.getElementById('ponto'))
+
     if(jogador1 == cpu){
         return 'Draw!'
     } else if (jogador1 == 1 && cpu == 2){
-        pontos--
+        pontos = pontos - 1
         return 'You Lose!'
     }else if (jogador1 == 1 && cpu == 3){
-        pontos++
+        pontos = pontos + 1
         return 'You Win!'
     }else if (jogador1 == 2 && cpu == 1){
-        pontos++
+        pontos = pontos + 1
         return 'You Win!'
     }else if (jogador1 == 2 && cpu == 3){
-        pontos--
+        pontos = pontos - 1
         return 'You Lose!'
     }else if (jogador1 == 3 && cpu == 1){
-        pontos--
+        pontos = pontos - 1
         return 'You Lose!'
     }else if (jogador1 == 3 && cpu == 2){
-        pontos++
+        pontos = pontos + 1
         return 'You Win!'
     }
 }
+
+console.log(jokenpo(1));
+console.log(pontos);
