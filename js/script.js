2ce2d47c27a04ed83189ffe10906c6c3fe74fc06
Manoel Gildarc <Manoelgildarc@hotmail.com>
2023-07-06 20:37:42 -0300

    Inserido document. getelement nos ifs.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-let pontos = 0 /* parseFloat(document.getElementById('ponto')) */
+let pontos = parseFloat(document.getElementById('ponto'))
 
 function jokenpo(jogador1){
     let cpu = Math.floor(Math.random() * (3 - 1 + 1)) + 1
 
     if(jogador1 == cpu){
-        return 'Draw!'
+        document.getElementById('resultado') = 'Draw!'
     } else if (jogador1 == 1 && cpu == 2){
-        pontos = pontos - 1
-        return 'You Lose!'
+        document.getElementById('ponto') = pontos - 1
+        document.getElementById('resultado') = 'You Lose!'
     }else if (jogador1 == 1 && cpu == 3){
-        pontos = pontos + 1
-        return 'You Win!'
+        document.getElementById('ponto') = pontos + 1
+        document.getElementById('resultado') = 'You Win!'
     }else if (jogador1 == 2 && cpu == 1){
-        pontos = pontos + 1
-        return 'You Win!'
+        document.getElementById('ponto') = pontos + 1
+        document.getElementById('resultado') = 'You Win!'
     }else if (jogador1 == 2 && cpu == 3){
-        pontos = pontos - 1
-        return 'You Lose!'
+        document.getElementById('ponto') = pontos - 1
+        document.getElementById('resultado') = 'You Lose!'
     }else if (jogador1 == 3 && cpu == 1){
-        pontos = pontos - 1
-        return 'You Lose!'
+        document.getElementById('ponto') = pontos - 1
+        document.getElementById('resultado') = 'You Lose!'
     }else if (jogador1 == 3 && cpu == 2){
-        pontos = pontos + 1
-        return 'You Win!'
+        document.getElementById('ponto') = pontos + 1
+        document.getElementById('resultado') = 'You Win!'
     }
 }
