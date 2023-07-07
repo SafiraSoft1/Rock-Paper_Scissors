a0aabc5e165e84be634ec1caed9f0021fc8f72e1
Manoel Gildarc <Manoelgildarc@hotmail.com>
2023-07-06 22:10:33 -0300

    Ajust. para alterar o placar, mas não está ok.

--- js/script.js
+++ js/script.js
@@ -4,24 +4,25 @@ function jokenpo(jogador1){
     let cpu = Math.floor(Math.random() * (3 - 1 + 1)) + 1
 
     if(jogador1 == cpu){
-        document.getElementById('resultado') = 'Draw!'
+        /* document.getElementById('resultado') = 'Draw!' */
     } else if (jogador1 == 1 && cpu == 2){
-        document.getElementById('ponto') = pontos - 1
-        document.getElementById('resultado') = 'You Lose!'
+        pontos --
+        /* document.getElementById('resultado') = 'You Lose!' */
     }else if (jogador1 == 1 && cpu == 3){
-        document.getElementById('ponto') = pontos + 1
-        document.getElementById('resultado') = 'You Win!'
+        pontos ++
+        /* document.getElementById('resultado') = 'You Win!' */
     }else if (jogador1 == 2 && cpu == 1){
-        document.getElementById('ponto') = pontos + 1
-        document.getElementById('resultado') = 'You Win!'
+        pontos++
+        /* document.getElementById('resultado') = 'You Win!' */
     }else if (jogador1 == 2 && cpu == 3){
-        document.getElementById('ponto') = pontos - 1
-        document.getElementById('resultado') = 'You Lose!'
+        pontos--
+        /* document.getElementById('resultado') = 'You Lose!' */
     }else if (jogador1 == 3 && cpu == 1){
-        document.getElementById('ponto') = pontos - 1
-        document.getElementById('resultado') = 'You Lose!'
+        pontos--
+        /* document.getElementById('resultado') = 'You Lose!' */
     }else if (jogador1 == 3 && cpu == 2){
-        document.getElementById('ponto') = pontos + 1
-        document.getElementById('resultado') = 'You Win!'
+        pontos++
+        /* document.getElementById('resultado') = 'You Win!' */
     }
+    document.getElementById('ponto').textContent = pontos
 }
