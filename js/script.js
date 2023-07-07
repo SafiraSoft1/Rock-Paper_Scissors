86b51b60203343838039ea9d6acbde3b03a95c10
Manoel Gildarc <Manoelgildarc@hotmail.com>
2023-07-07 20:56:09 -0300

    Criado variavel resultado mas não está ok.

--- js/script.js
+++ js/script.js
@@ -2,27 +2,27 @@ let pontos = document.getElementById('ponto')
 
 function jokenpo(jogador1){
     let cpu = Math.floor(Math.random() * (3 - 1 + 1)) + 1
-
+    let resultado = document.getElementById('resultado')
     if(jogador1 == cpu){
-        /* document.getElementById('resultado') = 'Draw!' */
+        resultado.textContent = 'Draw!'
     } else if (jogador1 == 1 && cpu == 2){
         pontos --
-        /* document.getElementById('resultado') = 'You Lose!' */
+        resultado.textContent = 'You Lose!'
     }else if (jogador1 == 1 && cpu == 3){
         pontos ++
-        /* document.getElementById('resultado') = 'You Win!' */
+        resultado.textContent = 'You Win!'
     }else if (jogador1 == 2 && cpu == 1){
         pontos++
-        /* document.getElementById('resultado') = 'You Win!' */
+        resultado.textContent = 'You Win!'
     }else if (jogador1 == 2 && cpu == 3){
         pontos--
-        /* document.getElementById('resultado') = 'You Lose!' */
+        resultado.textContent = 'You Lose!'
     }else if (jogador1 == 3 && cpu == 1){
         pontos--
-        /* document.getElementById('resultado') = 'You Lose!' */
+        resultado.textContent = 'You Lose!'
     }else if (jogador1 == 3 && cpu == 2){
         pontos++
-        /* document.getElementById('resultado') = 'You Win!' */
+        resultado.textContent = 'You Win!'
     }
     document.getElementById('ponto').textContent = pontos
 }
