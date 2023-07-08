4b2b5300890fe83d90e816e64fbde473cc2299a4
Manoel Gildarc <Manoelgildarc@hotmail.com>
2023-07-08 16:05:14 -0300

    Tratado erro de score ficar negativo.

--- js/script.js
+++ js/script.js
@@ -4,25 +4,28 @@ function jokenpo(jogador1){
     let cpu = Math.floor(Math.random() * (3 - 1 + 1)) + 1
     let resultado = document.getElementById('resultado')
     if(jogador1 == cpu){
-        resultado.textContent = 'Draw!'
+        /* resultado.textContent = 'Draw!' */
     } else if (jogador1 == 1 && cpu == 2){
         pontos --
-        resultado.textContent = 'You Lose!'
+        /* resultado.textContent = 'You Lose!' */
     }else if (jogador1 == 1 && cpu == 3){
         pontos ++
-        resultado.textContent = 'You Win!'
+        /* resultado.textContent = 'You Win!' */
     }else if (jogador1 == 2 && cpu == 1){
         pontos++
-        resultado.textContent = 'You Win!'
+        /* resultado.textContent = 'You Win!' */
     }else if (jogador1 == 2 && cpu == 3){
         pontos--
-        resultado.textContent = 'You Lose!'
+        /* resultado.textContent = 'You Lose!' */
     }else if (jogador1 == 3 && cpu == 1){
         pontos--
-        resultado.textContent = 'You Lose!'
+        /* resultado.textContent = 'You Lose!' */
     }else if (jogador1 == 3 && cpu == 2){
         pontos++
-        resultado.textContent = 'You Win!'
+        /* resultado.textContent = 'You Win!' */
     }
     document.getElementById('ponto').textContent = pontos
+    if(document.getElementById('ponto').textContent < 0){
+        document.getElementById('ponto').textContent = 0
+    }
 }
