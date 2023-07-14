8720a474189032631b8f60e4ea24bbe960aee7fd
Manoel Gildarc <Manoelgildarc@hotmail.com>
2023-07-13 22:15:45 -0300

    Carregar resultado criada. Mas falta ajustes

--- js/script.js
+++ js/script.js
@@ -20,40 +20,51 @@ document.addEventListener('DOMContentLoaded', function() {
     carregarConteudo();
 })
 
-
-
-
-
-
-
-
-
-
 let pontos = document.getElementById('ponto')
 
 function jokenpo(jogador1){
+    let section = document.getElementById('conteudo');
+    function carregarResultado() {
+        fetch('../html/resultado.html')
+            .then(function(resposta) {
+                if (resposta.ok) {
+                    return resposta.text();
+                } else {
+                    throw new Error('Erro ao carregar HTML');
+                }
+            })
+            .then(function(telaResultado) {
+                section.innerHTML = telaResultado;
+            })
+            .catch(function(erro) {
+                console.error(erro);
+            });
+    }
+    carregarResultado();
+
+    
     let cpu = Math.floor(Math.random() * (3 - 1 + 1)) + 1
     let resultado = document.getElementById('resultado')
     if(jogador1 == cpu){
-        /* resultado.textContent = 'Draw!' */
+        resultado.textContent = 'Draw!'
     } else if (jogador1 == 1 && cpu == 2){
         pontos --
-        /* resultado.textContent = 'You Lose!' */
+        resultado.textContent = 'You Lose!'
     }else if (jogador1 == 1 && cpu == 3){
         pontos ++
-        /* resultado.textContent = 'You Win!' */
+        resultado.textContent = 'You Win!'
     }else if (jogador1 == 2 && cpu == 1){
         pontos++
-        /* resultado.textContent = 'You Win!' */
+        resultado.textContent = 'You Win!'
     }else if (jogador1 == 2 && cpu == 3){
         pontos--
-        /* resultado.textContent = 'You Lose!' */
+        resultado.textContent = 'You Lose!'
     }else if (jogador1 == 3 && cpu == 1){
         pontos--
-        /* resultado.textContent = 'You Lose!' */
+        resultado.textContent = 'You Lose!'
     }else if (jogador1 == 3 && cpu == 2){
         pontos++
-        /* resultado.textContent = 'You Win!' */
+        resultado.textContent = 'You Win!'
     }
     document.getElementById('ponto').textContent = pontos
     if(document.getElementById('ponto').textContent < 0){
