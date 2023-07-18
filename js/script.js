a34004a2c41b3c3822cf848014143898412361d3
Manoel Gildarc <Manoelgildarc@hotmail.com>
2023-07-17 22:07:20 -0300

    Analisado e reduzido 19 linhas de código.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let section = document.getElementById('conteudo');
-    function carregarConteudo() {
-        fetch('../html/escolha.html')
-            .then(function (resposta) {
-                if (resposta.ok) {
-                    return resposta.text();
-                } else {
-                    throw new Error('Erro ao carregar HTML');
-                }
-            })
-            .then(function (conteudo) {
-                section.innerHTML = conteudo;
-            })
-            .catch(function (erro) {
-                console.error(erro);
-            });
-    }
-    carregarConteudo();
+    playAgain()
 })
 
 let pontos = document.getElementById('ponto')
@@ -36,49 +19,50 @@ function jokenpo(jogador1) {
             .then(function (telaResultado) {
                 section.innerHTML = telaResultado;
                 let cpu = Math.floor(Math.random() * (3 - 1 + 1)) + 1
-                let resultado = document.getElementById('resultado')
                 if (jogador1 == 1 && cpu == 1) {
-                    resultado.textContent = 'Draw!'
+                    document.getElementById('resultado').textContent = 'Draw!'
                     document.getElementById('papel').getElementsByTagName('img')[0].src = '../images/icon-rock.svg'
                     document.getElementById('pedra').getElementsByTagName('img')[0].src = '../images/icon-rock.svg'
                 } else if (jogador1 == 1 && cpu == 2) {
                     pontos--
-                    resultado.textContent = 'You Lose!'
+                    document.getElementById('resultado').textContent = 'You Lose!'
                     document.getElementById('papel').getElementsByTagName('img')[0].src = '../images/icon-rock.svg'
                     document.getElementById('pedra').getElementsByTagName('img')[0].src = '../images/icon-paper.svg'
                 } else if (jogador1 == 1 && cpu == 3) {
                     pontos++
-                    resultado.textContent = 'You Win!'
+                    document.getElementById('resultado').textContent = 'You Win!'
                     document.getElementById('papel').getElementsByTagName('img')[0].src = '../images/icon-rock.svg'
                     document.getElementById('pedra').getElementsByTagName('img')[0].src = '../images/icon-scissors.svg'
                 } else if (jogador1 == 2 && cpu == 1) {
                     pontos++
-                    resultado.textContent = 'You Win!'
+                    document.getElementById('resultado').textContent = 'You Win!'
                     document.getElementById('papel').getElementsByTagName('img')[0].src = '../images/icon-paper.svg'
                     document.getElementById('pedra').getElementsByTagName('img')[0].src = '../images/icon-rock.svg'
                 } else if (jogador1 == 2 && cpu == 3) {
                     pontos--
-                    resultado.textContent = 'You Lose!'
+                    document.getElementById('resultado').textContent = 'You Lose!'
                     document.getElementById('papel').getElementsByTagName('img')[0].src = '../images/icon-paper.svg'
                     document.getElementById('pedra').getElementsByTagName('img')[0].src = '../images/icon-scissors.svg'
                 } else if (jogador1 == 3 && cpu == 1) {
                     pontos--
-                    resultado.textContent = 'You Lose!'
+                    document.getElementById('resultado').textContent = 'You Lose!'
                     document.getElementById('papel').getElementsByTagName('img')[0].src = '../images/icon-scissors.svg'
                     document.getElementById('pedra').getElementsByTagName('img')[0].src = '../images/icon-rock.svg'
                 } else if (jogador1 == 3 && cpu == 2) {
                     pontos++
-                    resultado.textContent = 'You Win!'
+                    document.getElementById('resultado').textContent = 'You Win!'
                     document.getElementById('papel').getElementsByTagName('img')[0].src = '../images/icon-scissors.svg'
                     document.getElementById('pedra').getElementsByTagName('img')[0].src = '../images/icon-paper.svg'
                 } else if (jogador1 == 2 && cpu == 2) {
-                    resultado.textContent = 'Draw!'
+                    document.getElementById('resultado').textContent = 'Draw!'
                     document.getElementById('papel').getElementsByTagName('img')[0].src = '../images/icon-paper.svg'
                     document.getElementById('pedra').getElementsByTagName('img')[0].src = '../images/icon-paper.svg'
                 } else if (jogador1 == 3 && cpu == 3) {
-                    resultado.textContent = 'Draw!'
+                    document.getElementById('resultado').textContent = 'Draw!'
                     document.getElementById('papel').getElementsByTagName('img')[0].src = '../images/icon-scissors.svg'
                     document.getElementById('pedra').getElementsByTagName('img')[0].src = '../images/icon-scissors.svg'
+                } else if (document.getElementById('resultado').textContent == 'Draw!'){
+                    pontos = pontos + 0
                 }
                 document.getElementById('ponto').textContent = pontos
             })
